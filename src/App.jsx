53bd24b592d960ab2989429bb370e67e44e6f4f4
Fickery <fickery901@gmail.com>
2023-10-08 13:05:19 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+
+const POSTS = [
+  { id: 1, title: "React" },
+  { id: 2, title: "Vite" },
+];
+
+function App() {
+  const queryClient = useQueryClient();
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    // queryFn: () => Promise.reject("error"),
+    queryFn: () => wait(1000).then(() => [...POSTS]),
+  });
+
+  const newPostMutation = useMutation({
+    mutationFn: (title) => {
+      return wait(1000).then(() =>
+        POSTS.push({
+          id: crypto.randomUUID(),
+          title,
+        }),
+      );
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries("posts");
+    },
+  });
+
+  if (postsQuery.isLoading) return <h1>Loading...</h1>;
+  if (postsQuery.isError) {
+    return <pre>{JSON.stringify(postsQuery.error)}</pre>;
+  }
+
+  return (
+    <div>
+      {postsQuery.data.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+      <button
+        disabled={newPostMutation.isLoading}
+        onClick={() => newPostMutation.mutate("New Post")}
+      >
+        Add New
+      </button>
+    </div>
+  );
+}
+
+function wait(duration) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
+}
+
+export default App;
